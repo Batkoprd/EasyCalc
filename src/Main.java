37e21148faa17036de7ff9ed9eb49458eb4876d8
Ruben <batkoprd@gmail.com>
2022-06-30 23:25:37 +0300

    Просто

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+        String userInput = scanner.nextLine();
+
+        try {
+            System.out.println(Calc(userInput));
+        } catch (NumberFormatException e) {
+            System.out.println("Error! Not number");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Operation Error!");
+        } catch (ArithmeticException e) {
+            System.out.println("Error! Division by zero");
+        }
+    }
+
+
+    public static double Calc(String input) {
+        String numRegex = "-?(\\d+)\\.?(\\d+)?";
+        String operationRegex = "[+\\-*/]";
+
+        String[] arr = input.split(" ");
+        Double num1 = Double.parseDouble(arr[0]);
+        Double num2 = Double.parseDouble(arr[2]);
+        String operation = arr[1];
+
+        if (!arr[0].matches(numRegex) || !arr[2].matches(numRegex)) {
+            throw new NumberFormatException();
+        }
+        if (!operation.matches(operationRegex)) {
+            throw new IllegalArgumentException();
+        }
+
+        double result = 0.0;
+
+        switch (operation) {
+            case "+" -> result = num1 + num2;
+            case "-" -> result = num1 - num2;
+            case "*" -> result = num1 * num2;
+            case "/" -> {
+                if (num2 == 0) {
+                    throw new ArithmeticException();
+                }
+                result = num1 / num2;
+            }
+        }
+
+        if (result == -0.0) return 0;
+        return result;
+    }
+
+}
